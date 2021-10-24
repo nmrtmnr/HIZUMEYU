e8ecbdb41f5375784da13f85c2ec61be45e81e51
野村 朋範 <tomonori.nomura@i-studio.co.jp>
2021-10-24 16:06:42 +0900

    github actions nuxt generate 検証 01

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,12 +1,12 @@
 const contentful = require('contentful')
-const ctfConfig = require('../.contentful.json')
+// const ctfConfig = require('../.contentful.json')
 
 
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
 const config = {
-  space: ctfConfig.CTF_SPACE_ID,
-  accessToken: ctfConfig.CTF_CDA_ACCESS_TOKEN
+  space: process.env.CTF_SPACE_ID,
+  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
 }
 
 // export `createClient` to use it in page components
