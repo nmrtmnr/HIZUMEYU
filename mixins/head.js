15a11f2a193dae128237ce8f6ec0f88df0858568
Tomonori Nomura <>
2023-02-03 20:54:36 +0900

    head.js修正

--- mixins/head.js
+++ mixins/head.js
@@ -30,13 +30,13 @@ export default {
 
         { hid: 'description', name: 'description', content: DESCRIPTION },
         { hid: 'og:type', propery: 'og:type', content: TYPE },
-        { hid: 'og:title', propery: 'og:type', content: TITLE },
+        { hid: 'og:title', propery: 'og:title', content: TITLE },
         { hid: 'og:url', property: 'og:url', content: URL },
         { hid: 'og:description', property: 'og:description', content: DESCRIPTION },
         { hid: 'og:image', property: 'og:image', content: (this.meta && this.meta.image) || 'https//example.com/ogImagePath.png' },
         { hid: 'og:site_name', property: 'og:site_name', content: SITE_NAME },
 
-        { hid: 'twitter:card', name: 'twitter:cpard', content: 'summary_large_image' },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
       ],
       link: [
         { rel: 'shortcut icon', type: 'image/png', href: '/images/meta/icon.png' },
